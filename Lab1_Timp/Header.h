ce643c98e34e2cb62d4d10ce97cc3e0b8b3bd657
ArawaluProMaxNightWolfy <100471474+ArawaluProMaxNightWolfy@users.noreply.github.com>
2022-12-03 03:39:46 +0300

    Update Header.h

--- Lab1_Timp/Header.h
+++ Lab1_Timp/Header.h
@@ -1,1 +1,17 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
 
+//Ниже находится то, что надо поместить в header.h
+class TablMapReplace
+{
+private:
+    int main_key;
+public:
+    TablMapReplace(int a) {
+        main_key = a;
+    }
+    string decrypt(string messageLocal);
+    string encrypt(string messageLocal);
+};
