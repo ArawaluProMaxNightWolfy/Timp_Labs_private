d76ca8a6e7d0178e71fd74c6c2927166cda90f59
ArawaluProMaxNightWolfy <100471474+ArawaluProMaxNightWolfy@users.noreply.github.com>
2022-12-03 03:40:42 +0300

    Update modules.cpp

--- Lab1_Timp/modules.cpp
+++ Lab1_Timp/modules.cpp
@@ -1,24 +1,40 @@
 #pragma once
 #include <iostream>
-#include <stdio.h>
 #include <string>
 using namespace std;
 
-string crypt(int key, string message[]) {
-    bool gameover = false;
-    string chifr[100];
-
-    while (!gameover) {
-        for (int i = key - 1; i >= key - key; i--) {
-            if (message[i] == "0") {
-                gameover = true;
-                break;
-            }
-            else {
-                chifr += message[i];
-            }
-            key += key;
+//Ниже находится то, что нужно поместить в modules.cpp
+string TablMapReplace::decrypt(string messageLocal) {
+    string message = messageLocal;
+    int key = main_key;
+    string shifr;
+	for (int i = 0; i <= message.length(); i += key + 1) {
+		int j = i + key;
+		if (j > message.length() - 1) {
+			j = message.length() - 1;
+		}
+		//Выше мы получили значения i(начала участка строки) и j(конца участка строки)
+        for(j; j >= i; j--) {
+            shifr.push_back(message[j]);
         }
-    }
-    return chifr;
+		//Переставляем символы по индексам, добавляя в shifr
+	}
+	return shifr;
+}
+string TablMapReplace::encrypt(string messageLocal) {
+    string message = messageLocal;
+    int key = main_key;
+    string shifr;
+	for (int i = 0; i <= message.length(); i += key + 1) {
+		int j = i + key;
+		if (j > message.length() - 1) {
+			j = message.length() - 1;
+		}
+		//Выше мы получили значения i(начала участка строки) и j(конца участка строки)
+        for(j; j >= i; j--) {
+            shifr.push_back(message[j]);
+        }
+		//Переставляем символы по индексам, добавляя в shifr
+	}
+	return shifr;
 }
