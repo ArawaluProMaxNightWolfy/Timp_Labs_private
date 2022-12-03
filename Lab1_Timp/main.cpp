0963634fb2939f66ff7871489d8f067c83805887
ArawaluProMaxNightWolfy <100471474+ArawaluProMaxNightWolfy@users.noreply.github.com>
2022-12-03 03:38:51 +0300

    Update main.cpp

--- Lab1_Timp/main.cpp
+++ Lab1_Timp/main.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
+#include <string>
+#include "Header.h"
 #include "modules.cpp"
+using namespace std;
 
-int main() {
-    cout << crypt(5, "cool super puper0") << endl;
+int main(){
+    bool gameover = false;
+    int key;                          //Запрос ключа
+    cout << "Введите ключ: ";
+    cin >> key;
+    TablMapReplace perestanovka(key); //_________
+    
+    string symbol;
+    string message;
+    while(gameover != true) {
+        cout << "Поменять ключ(k), Зашифровать сообщение(e), Расшифровать сообщение(d)" << endl;
+        cin >> symbol;
+        if (symbol == "k") {
+            cout << "Введите ключ: ";
+            cin >> key;
+            TablMapReplace perestanovka(key);
+        }
+        if (symbol == "e") {
+            cout << "Введите сообщение: ";
+            cin >> message;
+            cout << perestanovka.encrypt(message) << endl;
+        }
+        if (symbol == "d") {
+            cout << "Введите сообщение: ";
+            cin >> message;
+            cout << perestanovka.decrypt(message) << endl;
+        } 
+        if (symbol != "k" and symbol != "e" and symbol != "d") {
+            cout << "Хе-хе, думали, я не предусмотрел выход?" << endl;
+            gameover = true;
+        }
+    }
     return 0;
 }
