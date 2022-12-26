bb9c55a9008fb53092f93c8d414452a4c21e1be5
ArawaluProMaxNightWolfy <100471474+ArawaluProMaxNightWolfy@users.noreply.github.com>
2022-12-26 04:25:52 +0300

    Update main.cpp

--- Lab1_Timp/main.cpp
+++ Lab1_Timp/main.cpp
@@ -1,28 +1,34 @@
 #include <iostream>
+#include <cctype>
 #include "modAlphaCipher.h"
 using namespace std;
-void check(const wstring& Text, const wstring& key)
-{
-    wstring cipherText;
-    wstring decryptedText;
-    modAlphaCipher cipher(key);
-    cipherText = cipher.encrypt(Text);
-    decryptedText = cipher.decrypt(cipherText);
-    wcout<<"key="<<key<<endl;
-    wcout<<Text<<endl;
-    wcout<<cipherText<<endl;
-    wcout<<decryptedText<<endl;
-    if (Text == decryptedText)
-        wcout<<"Ok\n";
-    else
-        wcout<<"Err\n";
-}
 int main(int argc, char **argv)
 {
-    std::locale loc("ru_RU.UTF-8");
-    std::locale::global(loc); // установка глобальной локали
-    check(L"ЗЕЛЕНАЯЗЕЛЕНЬЗЕЛЕНИТЗЕЛЕНУЮЗЕЛЕНЬ",L"СИМПЛ");
-    check(L"ЗЕЛЕНАЯЗЕЛЕНЬЗЕЛЕНИТЗЕЛЕНУЮЗЕЛЕНЬ",L"ПРИМЕР");
-    check(L"ЗЕЛЕНАЯЗЕЛЕНЬЗЕЛЕНИТЗЕЛЕНУЮЗЕЛЕНЬ",L"АПЧХИ");
+    int key;
+    string text;
+    unsigned op;
+    cout<<"Cipher ready. Input key: ";
+    cin>>key;
+    cout<<"Key loaded\n";
+    modAlphaCipher cipher(key);
+    do {
+        cout<<"Cipher ready. Input operation (0-exit, 1-encrypt, 2-decrypt): ";
+        cin>>op;
+        if (op > 2) {
+            cout<<"Illegal operation\n";
+        } else if (op >0) 
+            {
+            cout<<"Cipher ready. Input text: ";
+            cin>>text;
+            if (op==1) {
+                cout<<"Encrypted text: "<<cipher.encrypt(text)<<endl;
+            } else {
+                cout<<"Decrypted text: "<<cipher.decrypt(text)<<endl;
+            }
+        } else {
+            cout<<"Operation aborted: invalid text\n";
+
+        }
+    } while (op!=0);
     return 0;
 }
