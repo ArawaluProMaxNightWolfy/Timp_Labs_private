29c6bff89677172e9055606ebba0dafe50bd898c
ArawaluProMaxNightWolfy <100471474+ArawaluProMaxNightWolfy@users.noreply.github.com>
2022-12-26 04:28:13 +0300

    Update modAlphaChipher.h

--- Lab1_Timp/modAlphaChipher.h
+++ Lab1_Timp/modAlphaChipher.h
@@ -1,19 +1,13 @@
 #pragma once
 #include <vector>
 #include <string>
-#include <map>
-class modAlphaCiphers
+class modAlphaCipher
 {
 private:
-    std::wstring numAlpha =
-        L"АБВГДЕЁЖЗИЙКЛМОНПРСТУФХЦЧЪЫЬЭЮЯ"; //алфавит по порядку
-    std::map <char,int> alphaNum; //ассоциативный массив "номер по символу"
-    std::vector <int> key; //ключ
-    std::vector<int> convert(const std::wstring& s); //преобразование строка-вектор
-    std::wstring convert(const std::vector<int>& v); //преобразование вектор-строка
+    int key; //ключ
 public:
     modAlphaCipher()=delete; //запретим конструктор без параметров
-    modAlphaCipher(const std::wstring& skey); //конструктор для установки ключа
-    std::wstring encrypt(const std::wstring& open_text); //зашифрование
-    std::wstring decrypt(const std::wstring& cipher_text);//расшифрование
+    modAlphaCipher(const int skey); //конструктор для установки ключа
+    std::string encrypt(const std::string& text); //зашифрование
+    std::string decrypt(const std::string& dctext);//расшифрование
 };
